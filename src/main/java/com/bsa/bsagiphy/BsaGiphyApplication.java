95bcc7aae1a4f100f233f9606212ae2fccf75d71
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-21 16:38:24 +0300

    Init commit

---
+++ src/main/java/com/bsa/bsagiphy/BsaGiphyApplication.java
@@ -0,0 +1,13 @@
+package com.bsa.bsagiphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaGiphyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsaGiphyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsa/bsagiphy/BsaGiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsa.bsagiphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsaGiphyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
