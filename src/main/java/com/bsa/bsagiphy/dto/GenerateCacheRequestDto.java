8562cd8cc31139527efb14eebe8baa28e9501403
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 20:50:28 +0300

    Create GenerateCacheRequestDto

---
+++ src/main/java/com/bsa/bsagiphy/dto/GenerateCacheRequestDto.java
@@ -0,0 +1,10 @@
+package com.bsa.bsagiphy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GenerateCacheRequestDto {
+    public final String query;
+}
