b7d107d55f5f99f839aecf47d2b226adeac11429
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 19:57:05 +0300

    Change type of field id from UUID to String

--- src/main/java/com/bsa/bsagiphy/entity/Gif.java
+++ src/main/java/com/bsa/bsagiphy/entity/Gif.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Gif {
 
-    private UUID id;
+    private String id;
     private String name;
     private String path;
 }
