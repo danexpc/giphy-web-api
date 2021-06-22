b259457ca465dc466304ae39d7d89072eeb670be
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 20:04:53 +0300

    Create ApiConfiguration class

---
+++ src/main/java/com/bsa/bsagiphy/config/ApiConfiguration.java
@@ -0,0 +1,17 @@
+package com.bsa.bsagiphy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.net.http.HttpClient;
+
+@Configuration
+public class ApiConfiguration {
+
+    @Bean
+    @Scope(value = "prototype")
+    public HttpClient httpClient() {
+        return HttpClient.newHttpClient();
+    }
+}
