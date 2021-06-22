4a120e438c9f0f0d3058c37f8a398ce84718f269
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 20:24:00 +0300

    Change @Data annotation to @Getter

--- src/main/java/com/bsa/bsagiphy/dto/GifResponseDto.java
+++ src/main/java/com/bsa/bsagiphy/dto/GifResponseDto.java
@@ -2,8 +2,9 @@ package com.bsa.bsagiphy.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class GifResponseDto {
     private final String path;
