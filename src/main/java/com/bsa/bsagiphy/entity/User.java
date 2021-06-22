73b9953e272bb19f6033418b3c2cd7c9c8f8effc
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 20:18:47 +0300

    Change type of id field from UUID to String

--- src/main/java/com/bsa/bsagiphy/entity/User.java
+++ src/main/java/com/bsa/bsagiphy/entity/User.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    private UUID id;
+    private String id;
 }
