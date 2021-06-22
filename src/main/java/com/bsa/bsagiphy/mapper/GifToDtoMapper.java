585ce666b67eea3cf6a7a480e35e6e049635ab60
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 11:56:05 +0300

    Add mapCollection method to mapper

--- src/main/java/com/bsa/bsagiphy/mapper/GifToDtoMapper.java
+++ src/main/java/com/bsa/bsagiphy/mapper/GifToDtoMapper.java
@@ -3,11 +3,19 @@ package com.bsa.bsagiphy.mapper;
 import com.bsa.bsagiphy.dto.GifResponseDto;
 import com.bsa.bsagiphy.entity.Gif;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GifToDtoMapper implements Mapper<GifResponseDto, Gif> {
     @Override
     public GifResponseDto map(Gif entity) {
         return new GifResponseDto(
                 entity.getUrl()
         );
+    }
+
+    @Override
+    public List<GifResponseDto> mapCollection(List<Gif> gifs) {
+        return gifs.stream().map(this::map).collect(Collectors.toList());
     }
 }
--- src/main/java/com/bsa/bsagiphy/mapper/Mapper.java
+++ src/main/java/com/bsa/bsagiphy/mapper/Mapper.java
@@ -1,5 +1,9 @@
 package com.bsa.bsagiphy.mapper;
 
+import java.util.List;
+
 public interface Mapper<T, U> {
     T map(U entity);
+
+    List<T> mapCollection(List<U> entities);
 }
