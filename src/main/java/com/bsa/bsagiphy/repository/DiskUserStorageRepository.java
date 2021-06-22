201ed14bd3f3bf78802669aa238f41f2f37860c5
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 22:34:51 +0300

    Create DiskUserStorageRepository

---
+++ src/main/java/com/bsa/bsagiphy/repository/DiskUserStorageRepository.java
@@ -0,0 +1,8 @@
+package com.bsa.bsagiphy.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DiskUserStorageRepository implements GifRepository {
+    // todo
+}
