8fbcac932e75dfff3766cf0242c802421f22ac98
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 12:01:29 +0300

    Mark as a component

--- src/main/java/com/bsa/bsagiphy/mapper/GifToDtoMapper.java
+++ src/main/java/com/bsa/bsagiphy/mapper/GifToDtoMapper.java
@@ -2,10 +2,12 @@ package com.bsa.bsagiphy.mapper;
 
 import com.bsa.bsagiphy.dto.GifResponseDto;
 import com.bsa.bsagiphy.entity.Gif;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class GifToDtoMapper implements Mapper<GifResponseDto, Gif> {
     @Override
     public GifResponseDto map(Gif entity) {
