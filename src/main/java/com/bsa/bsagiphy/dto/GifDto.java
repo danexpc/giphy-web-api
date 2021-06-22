654a1b1677dc8e96f4bfc0aa800274d2779af801
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 11:40:58 +0300

    Create GifDto

---
+++ src/main/java/com/bsa/bsagiphy/dto/GifDto.java
@@ -0,0 +1,11 @@
+package com.bsa.bsagiphy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GifDto {
+    private final String name;
+    private final String url;
+}
