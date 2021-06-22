245076dd36726074091aa9c983e292da9d9fb8b6
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-22 22:06:16 +0300

    Create function get() that invoke function getGif() of httpGifsApiClient

--- src/main/java/com/bsa/bsagiphy/service/GifOperationService.java
+++ src/main/java/com/bsa/bsagiphy/service/GifOperationService.java
@@ -1,8 +1,22 @@
 package com.bsa.bsagiphy.service;
 
+import com.bsa.bsagiphy.entity.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GifOperationService {
-    // todo
+
+    @Autowired
+    private HttpGifsApiClient httpGifsApiClient;
+
+    public List<Gif> getAll() {
+        return null;
+    }
+
+    public Gif get(String query) {
+        return httpGifsApiClient.getGif(query);
+    }
 }
