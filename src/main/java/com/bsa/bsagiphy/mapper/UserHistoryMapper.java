f5ef8dd6bd443a27ff13434cdd62e49746b3443a
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-23 20:24:47 +0300

    Add method mapper which maps List<UserHistory> to List<UserHistoryDto>

--- src/main/java/com/bsa/bsagiphy/mapper/UserHistoryMapper.java
+++ src/main/java/com/bsa/bsagiphy/mapper/UserHistoryMapper.java
@@ -4,10 +4,14 @@ import com.bsa.bsagiphy.dto.UserHistoryDto;
 import com.bsa.bsagiphy.entity.UserHistory;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserHistoryMapper {
 
     UserHistoryDto userHistoryToUserHistoryDto(UserHistory entity);
+
+    List<UserHistoryDto> listUserHistoryToListUserHistoryDto(List<UserHistory> entities);
 
     UserHistory userHistoryDtoToUserHistory(UserHistoryDto dto);
 }
