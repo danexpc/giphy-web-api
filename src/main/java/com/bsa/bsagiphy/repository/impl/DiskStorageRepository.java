9afda8304fa58d3ae7d4b76021905920d7c944d1
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-23 14:17:24 +0300

    Add methods that updates history and find gif by query

--- src/main/java/com/bsa/bsagiphy/repository/impl/DiskStorageRepository.java
+++ src/main/java/com/bsa/bsagiphy/repository/impl/DiskStorageRepository.java
@@ -1,9 +1,60 @@
 package com.bsa.bsagiphy.repository.impl;
 
+import com.bsa.bsagiphy.entity.Gif;
 import com.bsa.bsagiphy.repository.GifRepository;
+import com.bsa.bsagiphy.util.OperationsWithFileSystem;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import static com.bsa.bsagiphy.util.OperationsWithFileSystem.getRandomFileFromDirectory;
 
 @Repository
 public class DiskStorageRepository implements GifRepository {
-    // todo
+
+    @Value("${resources.location.cache}")
+    private String pathToCache;
+
+    @Value("${resources.location.users}")
+    private String pathToUsersStorage;
+
+    @Value("${resources.location.users.history-file-name}")
+    private String historyFileName;
+
+    public Optional<Gif> getGifByQuery(String query, String userId) {
+        var sourceDir = Path.of(pathToCache + query);
+        Optional<File> maybeFile = getRandomFileFromDirectory(sourceDir);
+        maybeFile.ifPresent(file -> OperationsWithFileSystem.copyFile(file,
+                new File(pathToUsersStorage + userId + File.separator + file.getName())));
+
+        if (maybeFile.isPresent()) {
+            var file = maybeFile.get();
+            return Optional.of(new Gif(file.getName(), file.getName(), file.getPath()));
+        }
+
+        return Optional.empty();
+    }
+
+    public void updateHistory(String userId, String query, Gif gif) throws IOException {
+        try (var fileWriter = new FileWriter(pathToUsersStorage + userId + File.separator + historyFileName);
+             var printWriter = new PrintWriter(fileWriter)) {
+            printWriter.write(buildNewHistoryRecord(query, gif));
+        }
+    }
+
+    private String buildNewHistoryRecord(String query, Gif gif) {
+        var dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        String todayDate = LocalDate.now().format(dtf);
+        return todayDate + "," + query + "," + gif.getPath();
+    }
+
+
 }
