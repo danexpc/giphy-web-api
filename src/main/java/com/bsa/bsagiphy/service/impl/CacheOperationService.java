6526e2add754aad87f1d50ebe435b92bd327b524
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-23 11:07:05 +0300

    Create template methods in services

--- src/main/java/com/bsa/bsagiphy/service/impl/CacheOperationService.java
+++ src/main/java/com/bsa/bsagiphy/service/impl/CacheOperationService.java
@@ -1,8 +1,28 @@
 package com.bsa.bsagiphy.service.impl;
 
+import com.bsa.bsagiphy.entity.Cache;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CacheOperationService {
-    // todo
+    public Cache getCacheByQuery(String query) {
+        // todo
+        return null;
+    }
+
+    public List<Cache> getAllCache() {
+        // todo
+        return null;
+    }
+
+    public Cache createGifInCache() {
+        // todo
+        return null;
+    }
+
+    public void deleteCache() {
+        // todo
+    }
 }
--- src/main/java/com/bsa/bsagiphy/service/impl/UserOperationService.java
+++ src/main/java/com/bsa/bsagiphy/service/impl/UserOperationService.java
@@ -1,8 +1,48 @@
 package com.bsa.bsagiphy.service.impl;
 
+import com.bsa.bsagiphy.dto.GenerateGifForUserDto;
+import com.bsa.bsagiphy.entity.Cache;
+import com.bsa.bsagiphy.entity.Gif;
+import com.bsa.bsagiphy.entity.UserHistory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserOperationService {
-    // todo
+    public List<Cache> getAllPersonalFilesByUserId(String id) {
+        // todo
+        return null;
+    }
+
+    public UserHistory getHistoryByUserId(String id) {
+        // todo
+        return null;
+    }
+
+    public void cleanHistoryByUserId(String id) {
+        // todo
+    }
+
+    public Gif searchGifByQuery(String id, String query, Boolean force) {
+        // todo
+        return null;
+    }
+
+    public Gif createGif(String id, GenerateGifForUserDto dto) {
+        // todo
+        return null;
+    }
+
+    public void resetUserCacheByQuery(String id, String query) {
+        // todo
+    }
+
+    public void resetAllUserCache(String id) {
+        // todo
+    }
+
+    public void cleanAllUserData(String id) {
+        // todo
+    }
 }
