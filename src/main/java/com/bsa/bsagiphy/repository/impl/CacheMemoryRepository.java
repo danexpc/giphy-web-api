53419e7eaba10cc88d3b53f1863c2f1c3d61a75d
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-23 14:16:07 +0300

    Create method which updates cache in memory

--- src/main/java/com/bsa/bsagiphy/repository/impl/CacheMemoryRepository.java
+++ src/main/java/com/bsa/bsagiphy/repository/impl/CacheMemoryRepository.java
@@ -1,9 +1,36 @@
 package com.bsa.bsagiphy.repository.impl;
 
+import com.bsa.bsagiphy.entity.Cache;
+import com.bsa.bsagiphy.entity.Gif;
 import com.bsa.bsagiphy.repository.GifRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 @Repository
 public class CacheMemoryRepository implements GifRepository {
-    // todo
+
+    private static final Map<String, List<Cache>> cache = new HashMap<>();
+
+    public void updateCache(String userId, String query, Gif gif) {
+        var userCache = cache.getOrDefault(userId, new ArrayList<>());
+        Optional<Cache> maybeCache = userCache.stream().filter(c -> Objects.equals(c.getQuery(), query)).findFirst();
+        if (maybeCache.isPresent()) {
+            var oldCache = maybeCache.get();
+            var gifs = oldCache.getGifs();
+            gifs.add(gif);
+            var newCache = new Cache(query, gifs);
+            userCache.replaceAll(c -> {
+                if (Objects.equals(c.getQuery(), query)) {
+                    return newCache;
+                }
+                return c;
+            });
+            cache.put(userId, userCache);
+        } else {
+            var gifs = new ArrayList<Gif>();
+            gifs.add(gif);
+            userCache.add(new Cache(query, gifs));
+        }
+    }
 }
