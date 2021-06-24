a6b3b87df0efcbd6480310d88ede1fc2865b010d
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-24 15:06:40 +0300

    Create configuration file for interceptors

---
+++ src/main/java/com/bsa/bsagiphy/config/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package com.bsa.bsagiphy.config;
+
+import com.bsa.bsagiphy.interceptor.RequestHandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    RequestHandlerInterceptor requestHandlerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestHandlerInterceptor);
+    }
+}
