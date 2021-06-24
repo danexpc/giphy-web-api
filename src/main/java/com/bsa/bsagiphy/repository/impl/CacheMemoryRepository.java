a3438801bca6d8d5af3380e3971a26a70d61ebcd
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-24 12:57:22 +0300

    Add methods which delete users cache

--- src/main/java/com/bsa/bsagiphy/repository/impl/CacheMemoryRepository.java
+++ src/main/java/com/bsa/bsagiphy/repository/impl/CacheMemoryRepository.java
@@ -3,17 +3,19 @@ package com.bsa.bsagiphy.repository.impl;
 import com.bsa.bsagiphy.entity.Cache;
 import com.bsa.bsagiphy.entity.Gif;
 import com.bsa.bsagiphy.repository.GifRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+@Log4j2
 @Repository
 public class CacheMemoryRepository implements GifRepository {
 
     private static final Map<String, List<Cache>> cache = new HashMap<>();
 
     public Optional<Gif> getFileByQuery(String userId, String query) {
-        System.out.println(cache);
         var userCache = cache.get(userId);
         if (userCache == null)
             return Optional.empty();
@@ -32,25 +34,48 @@ public class CacheMemoryRepository implements GifRepository {
     }
 
     public void updateCache(String userId, String query, Gif gif) {
+        log.error(cache);
         var userCache = cache.getOrDefault(userId, new ArrayList<>());
         var maybeCache = userCache.stream().filter(c -> Objects.equals(c.getQuery(), query)).findFirst();
         if (maybeCache.isPresent()) {
             var oldCache = maybeCache.get();
             var gifs = oldCache.getGifs();
-            gifs.add(gif.getPath());
-            var newCache = new Cache(query, gifs);
-            userCache.replaceAll(c -> {
-                if (Objects.equals(c.getQuery(), query)) {
-                    return newCache;
-                }
-                return c;
-            });
-            cache.put(userId, userCache);
+            if (!gifs.contains(gif.getPath())) {
+                gifs.add(gif.getPath());
+                var newCache = new Cache(query, gifs);
+                userCache.replaceAll(c -> {
+                    if (Objects.equals(c.getQuery(), query)) {
+                        return newCache;
+                    }
+                    return c;
+                });
+                cache.put(userId, userCache);
+            }
         } else {
             var gifs = new ArrayList<String>();
             gifs.add(gif.getPath());
             userCache.add(new Cache(query, gifs));
             cache.put(userId, userCache);
         }
+    }
+
+    public void deleteCache(String id) {
+        log.error("Before " + cache);
+        cache.put(id, new ArrayList<>());
+        log.error("After " + cache);
+    }
+
+    public void deleteCacheByQuery(String id, String query) {
+        log.error("Before " + cache);
+        var userCache = cache.getOrDefault(id, new ArrayList<>());
+        if (userCache.isEmpty()) {
+            return;
+        }
+
+        var filteredCache = userCache.stream()
+                .filter(c -> !c.getQuery().equals(query)).collect(Collectors.toList());
+
+        cache.put(id, filteredCache);
+        log.error("After " + cache);
     }
 }
